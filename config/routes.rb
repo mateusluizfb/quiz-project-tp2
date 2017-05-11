1ff7b5c89d6d5cad8d0ab270958173f6a33c393c
Mateus Luiz <mateuslfreitasb@gmail>
2017-05-11 20:47:29 -0300

    Topicos feitos

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :disciplines
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
