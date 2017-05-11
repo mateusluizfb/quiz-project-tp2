87e7e5f0ef334637525adc86b0da4795fca66ab7
Rafael Fernandes <spnrafael@gmail.com>
2017-05-11 17:54:04 -0300

    #6, #7, #8 e #9: criar, atualizar e excluir um usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :disciplines
-  has_and_belongs_to_many :teams
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # has_and_belongs_to_many :teams
+  # has_and_belongs_to_many :disciplines
 end
