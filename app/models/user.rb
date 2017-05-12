85ac1906c93aba495296d6ba9b7e2c6cbc713fb5
Rafael Fernandes <spnrafael@gmail.com>
2017-05-12 11:35:30 -0300

    Atualiza testes unitários de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name, :email, :login, :password
 
-  # has_and_belongs_to_many :teams
-  # has_and_belongs_to_many :disciplines
+  has_and_belongs_to_many :teams
+  has_and_belongs_to_many :disciplines
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,34 @@ RSpec.describe User, type: :model do
     user = User.new(name: 'user', login: 'user', email: 'user@example.com')
     expect(user).not_to be_valid
   end
+
+  it "belongs to many disciplines" do
+    user = User.new(name: 'user', login: 'user', email: 'user@example.com', password: 'user123')
+    user.save
+
+    discipline_1 = Discipline.create(name: 'discipline 1')
+    discipline_2 = Discipline.create(name: 'discipline 2')
+
+    user.disciplines << discipline_1
+    user.disciplines << discipline_2
+
+    user.save
+
+    expect(user.disciplines.count).to eq(2)
+  end
+
+  it "belongs to many teams" do
+    user = User.new(name: 'user', login: 'user', email: 'user@example.com', password: 'user123')
+    user.save
+
+    team_1 = Team.create(name: 'team 1')
+    team_2 = Team.create(name: 'team 2')
+
+    user.teams << team_1
+    user.teams << team_2
+
+    user.save
+
+    expect(user.teams.count).to eq(2)
+  end
 end
