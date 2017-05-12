2afbc5782257910af8fb206216110a90a6ccbed4
Rafael Fernandes <spnrafael@gmail.com>
2017-05-12 11:37:47 -0300

    Atualiza testes unitários de disciplina

--- spec/models/discipline_spec.rb
+++ spec/models/discipline_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Discipline, type: :model do
     discipline = Discipline.new
     expect(discipline).not_to be_valid
   end
+
+  it "belongs to many users" do
+    discipline = Discipline.new(name: 'new discipline')
+    discipline.save
+
+    user1 = User.create(name: 'user 1', login: 'user 1', email: 'user1@example.com', password: 'user123')
+    user2 = User.create(name: 'user 2', login: 'user 2', email: 'user2@example.com', password: 'user123')
+  
+    discipline.users << user1
+    discipline.users << user2
+
+    discipline.save
+
+    expect(discipline.users.count).to eq(2)
+  end
 end
