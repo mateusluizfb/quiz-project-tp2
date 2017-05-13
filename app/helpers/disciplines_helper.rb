e6ab7235d84b34fbcecf6751be9adfb6b0bd3704
Rafael Fernandes <spnrafael@gmail.com>
2017-05-12 23:14:35 -0300

    Atualiza exportação de disciplinas

--- app/helpers/disciplines_helper.rb
+++ app/helpers/disciplines_helper.rb
@@ -3,7 +3,21 @@ module DisciplinesHelper
     file = File.new('public/report.txt', 'w')
 
     discipline.topics.each_with_index do |topic, index|
-      file.puts("0.#{index + 1}|#{discipline.name}|#{topic.name}")
+      unless topic.quizzes.empty?
+        quizzes = "{"
+
+        topic.quizzes.each do |quiz|
+          unless topic.quizzes.last == quiz
+            quizzes += "#{quiz.name}, "
+          else
+            quizzes += quiz.name
+          end
+        end
+
+        quizzes += "}"
+      end
+
+      file.puts("0.#{index + 1}|#{discipline.name}|#{topic.name}|#{quizzes}")
     end
 
     file.close
@@ -16,7 +30,21 @@ module DisciplinesHelper
 
     disciplines.each_with_index do |discipline, d_index|
       discipline.topics.each_with_index do |topic, t_index|
-        file.puts("#{d_index}.#{t_index + 1}|#{discipline.name}|#{topic.name}")
+        unless topic.quizzes.empty?
+          quizzes = "{"
+
+          topic.quizzes.each do |quiz|
+            unless topic.quizzes.last == quiz
+              quizzes += "#{quiz.name}, "
+            else
+              quizzes += quiz.name
+            end
+          end
+
+          quizzes += "}"
+        end
+
+        file.puts("#{d_index}.#{t_index + 1}|#{discipline.name}|#{topic.name}|#{quizzes}")
       end
     end
 
