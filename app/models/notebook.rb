3d813b545fa43e4e26222ac1210269332ee37180
Mateus Luiz <mateuslfreitasb@gmail>
2017-06-22 10:44:45 -0300

    Notebook specs

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,4 +1,6 @@
 class Notebook < ApplicationRecord
+  validates_presence_of :user_id
+
   belongs_to :user
   has_and_belongs_to_many :disciplines
 end
--- spec/models/notebook_spec.rb
+++ spec/models/notebook_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Notebook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) {
+    User.create!(
+      name: 'Fulano',
+      login: 'fulano123',
+      email: 'email@email.com',
+      password: '123456',
+    )
+  }
+
+  let(:valid_attributes) {
+    {
+      user_id: user.id
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      user_id: nil
+    }
+  }
+
+  it 'Is valid with valid attributes' do
+    notebook = Notebook.new valid_attributes
+    expect(notebook).to be_valid
+  end
+
+  it 'Is invalid with invalid attributes' do
+    notebook = Notebook.new invalid_attributes
+    expect(notebook).to_not be_valid
+  end
+
+
 end
