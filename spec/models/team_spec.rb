4b93595dff766f8c8d620acba361ae6b553d2377
Mateus Luiz <mateuslfreitasb@gmail>
2017-06-22 10:53:34 -0300

    Team specs

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  it "is invalid without name" do
-    team = Team.new
-    expect(team).not_to be_valid
+
+  let(:valid_attributes) { { name: 'Sadboys' } }
+
+  let(:invalid_attributes) { { name: nil } }
+
+
+  it 'Is valid with valid attributes' do
+    team = Team.new valid_attributes
+    expect(team).to be_valid
   end
+
+  it 'Is invalid with invalid attributes' do
+    team = Team.new invalid_attributes
+    expect(team).to_not be_valid
+  end
+
 end
